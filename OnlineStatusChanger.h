64b8c478db3d8a40d9340d98ad551e3c7630212c
Balu3060 <balazs.sipos118@gmail.com>
2026-03-05 01:05:26 +0100

    Update OnlineStatusChanger.h

--- OnlineStatusChanger.h
+++ OnlineStatusChanger.h
@@ -1,9 +1,11 @@
 #pragma once
+#include <string>
+#include <functional>
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 #include "bakkesmod/plugin/pluginwindow.h"
 #include "bakkesmod/plugin/PluginSettingsWindow.h"
 
-class OnlineStatusChanger : public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginSettingsWindow
+class StatusOverrider : public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginSettingsWindow
 {
 public:
     virtual void onLoad() override;
