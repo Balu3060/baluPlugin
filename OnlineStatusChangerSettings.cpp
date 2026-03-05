c875bd1572b42d5b0cb03e3483de0129f67956f8
Balu3060 <balazs.sipos118@gmail.com>
2026-03-05 01:19:42 +0100

    Refactor OnlineStatusChanger to StatusOverrider

--- OnlineStatusChangerSettings.cpp
+++ OnlineStatusChangerSettings.cpp
@@ -1,33 +1,20 @@
 #include "OnlineStatusChanger.h"
 #include "imgui/imgui.h"
 
-void OnlineStatusChanger::SetImGuiContext(uintptr_t ctx)
-{
-    ImGui::SetCurrentContext(reinterpret_cast<ImGuiContext*>(ctx));
-}
+void StatusOverrider::SetImGuiContext(uintptr_t ctx) { ImGui::SetCurrentContext(reinterpret_cast<ImGuiContext*>(ctx)); }
+std::string StatusOverrider::GetPluginName() { return "Status Overrider"; }
 
-std::string OnlineStatusChanger::GetPluginName()
+void StatusOverrider::RenderSettings()
 {
-    return "Online Status Changer";
-}
-
-void OnlineStatusChanger::RenderSettings()
-{
-    CVarWrapper enableCvar = cvarManager->getCvar("cl_online_status_enable");
-    if (!enableCvar) return;
-
-    bool enabled = enableCvar.getBoolValue();
-    if (ImGui::Checkbox("Enable Plugin", &enabled))
-    {
-        enableCvar.setValue(enabled);
+    CVarWrapper enableCvar = cvarManager->getCvar("cl_status_enable");
+    if (enableCvar) {
+        bool enabled = enableCvar.getBoolValue();
+        if (ImGui::Checkbox("Enable Plugin", &enabled)) enableCvar.setValue(enabled);
     }
 
-    CVarWrapper statusCvar = cvarManager->getCvar("cl_online_status_override");
-    if (!statusCvar) return;
-
-    int statusValue = statusCvar.getIntValue();
-    if (ImGui::SliderInt("Status Override", &statusValue, 0, 3))
-    {
-        statusCvar.setValue(statusValue);
+    CVarWrapper statusCvar = cvarManager->getCvar("cl_status_override");
+    if (statusCvar) {
+        int statusValue = statusCvar.getIntValue();
+        if (ImGui::SliderInt("Status (0=Off, 1=Online, 2=Away, 3=Invisible)", &statusValue, 0, 3)) statusCvar.setValue(statusValue);
     }
 }
